7ebe97d60d6e3e8802d57bbf1dafa4e162e90131
zeeshanayaz <zeeshanayaz1@gmail.com>
2025-10-23 23:56:18 +0500

    FastAPI get post put delete method

--- FastAPI/Class_06/main.py
+++ FastAPI/Class_06/main.py
@@ -1,12 +1,15 @@
 from fastapi import FastAPI
+from schema.item import Item
 
+# Create instance for FastAPI
 app = FastAPI()
 
+# Health check API endpoint
 @app.get('/')
-def read_root():
+def health_check():
     return {
-        "Success": True,
-        "Message": "API is working properly"
+        "success": True,
+        "message": "API is working properly"
     }
 
 
@@ -17,9 +20,75 @@ def hello():
         "Message": "Hello, World!"
     }
 
+
 @app.get('/v1/hello/{name}')
 def hello(name: str):
     return {
         "Success": True,
         "Message": f"Hello, {name}!"
+    }
+
+
+
+items = [
+    Item(id=1, name='Mobile'),
+    Item(id=2, name='Laptop'),
+    Item(id=3, name='Headphone'),
+];
+
+
+# Get all items API endpoint
+@app.get('/v1/items')
+def get_items():
+    return {
+        "success": True,
+        "message": "Items fetched successfully",
+        "items": items
+    }
+
+
+# Get item at {item_id}
+@app.get('/v1/items/{item_id}')
+def get_item(item_id: int):
+    return {
+        "success": True,
+        "message": "Item fetched successfully",
+        "item": items[item_id]
+    }
+
+
+# Add item
+@app.post("/v1/item")
+def add_item(item_name: str):
+    item = Item(id=len(items)+1, name=item_name)
+    
+    items.append(item)
+    return {
+        "success": True,
+        "message": "Item fetched successfully",
+        "item": item
+    }
+
+
+# Update item
+@app.put('/v1/items/{item_id}')
+def update_item(item_id: int, item_name: str):
+    items[item_id-1] = Item(id=item_id, name=item_name)
+
+    return {
+        "success": True,
+        "message": "Item Updated successfully",
+        "item": items[item_id-1]
+    }
+
+
+# Delete item
+@app.delete('/v1/items/{item_id}')
+def delete_item(item_id: int):
+    item = items.pop(item_id-1)
+
+    return {
+        "success": True,
+        "message": "Item Updated successfully",
+        "item": item
     }
